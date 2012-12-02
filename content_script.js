a415cbc002fd1e11b2a09769268feb0d2b85ed1c
Alberto Alcolea <albertoalcolea@gmail.com>
2012-12-02 20:17:54 +0100

    funcionando ok, pero se muestra en todas las paginas (en los enlaces)

---
+++ content_script.js
@@ -0,0 +1,23 @@
+function searchPhoto() {
+  var query = document.querySelector("img#photo_image");
+  if (query) return query.src;
+}
+
+
+/*document.addEventListener("mousedown", function(event) {
+    //right click
+    if(event.button == 2) {
+    	var imgSrc = searchPhoto();
+    	chrome.extension.sendRequest({cmd: "crearMenu", imgSrc: imgSrc});
+    }
+}, true);*/
+
+
+
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	if (request.greeting == "hello") {
+  	  imgUrl = searchPhoto();
+      sendResponse({imageUrl: imgUrl});
+	}
+});
